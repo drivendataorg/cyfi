5e68b01e40d030702313a42d2ea3887f74c4272b
Katie Wetstone <klwetstone@gmail.com>
2023-07-19 11:11:59 -0400

    flesh out satellite code

--- cyano/data/satellite_data.py
+++ cyano/data/satellite_data.py
@@ -1,52 +1,108 @@
+from typing import List, Union
+
 from loguru import logger
 import pandas as pd
 from pathlib import Path
+import planetary_computer as pc
 from tqdm import tqdm
 
-# retuen to
-# - selectin speciifc items
-# - query through an specifying args
 
-
-def download_sample_imagery(
-    date, latitude, longitude, sources, time_window, geographic_window
-) -> pd.Dataframe:
-    """Query one sample based on its date, latitude, and longitude, and download the result.
+def search_planetary_computer(
+    date: Union[str, pd.Timestamp],
+    latitude: float,
+    longitude: float,
+    collections: List[str],
+    days_search_window: int,
+    meters_search_window: int,
+) -> List[pc.Item]:
+    """Search the planetary computer for imagery relevant to a given sample
 
     Args:
-        date (_type_): _description_
-        latitude (_type_): _description_
-        longitude (_type_): _description_
-        sources (_type_): _description_
-        time_window (_type_): _description_
-        geographic_window (_type_): _description_
+        date (Union[str, pd.Timestamp]): Sample date
+        latitude (float): Sample latitude
+        longitude (float): Sample longitude
+        collections (List[str]): Satellite imagery collections to search
+        days_search_window (int): Number of days before the sample to include
+            in the search
+        meters_search_window (int): Buffer in meters to add on each side of the
+            sample location when searching for imagery
 
     Returns:
-        pd.Dataframe: Dataframe listing all possible PySTAC items
-            to be included for the given sample
+        List[pc.Item]: List of all pystac items returned
     """
-
-    # Query planetary computer
-    return pd.DataFrame()
-
-
-def select_items(items: pd.DataFrame):
     pass
 
 
-def download_satellite_data(sample_list: pd.Dataframe, features_dir: Path):
-    """Query the planetary computer for Sentinel-2 and Landsat satellite
-    data, and save out the raw results.
+def select_items(
+    date: Union[str, pd.Timestamp], latitude: float, longitude: float, items: List[pc.Item]
+) -> List[pc.Item]:
+    """Select which pystac items to include for each sample
 
     Args:
-        sample_list (pd.Dataframe): Dataframe with columns for date,
-            longitude, latitude, and uid
-        features_dir (Path): Directory in which to save raw satellite data
+        date (Union[str, pd.Timestamp]): Sample date
+        latitude (float): Sample latitude
+        longitude (float): Sample longitude
+        items (List[pc.Item]): List of all pystac items returned by the planerary computer
+
+    Returns:
+        List[pc.Item]: List of pystac items to use when generating features for the sample
+    """
+    pass
+
+
+def save_item(item: pc.Item, save_dir: Path, sample_id: str):
+    """Save a pystac item
+
+    Args:
+        item (pc.Item): Item to save
+        save_dir (Path): Directory to save all raw source data
+        sample_id (str): ID of the sample for which the item will be
+            used when generating features
+    """
+    pass
+
+
+def download_satellite_data(
+    sample_list: pd.Dataframe,
+    save_dir: Path,
+    collections: List[str] = ["sentinel-2-l2a", "landsat-c2-l2"],
+    days_search_window: int = 15,
+    meters_search_window: int = 50000,
+):
+    """Query the planetary computer for satellite data for a set of sample,
+    select which items to include for each sample, and save out the raw results
+    for processing.
+
+    Args:
+        sample_list (pd.Dataframe): Dataframe where the index is uid and there are
+            columns for date, longitude, and latitude
+        save_dir (Path): Directory to save all raw source data
+        collections (List[str], optional): Satellite imagery collections to search in
+            the planetary computer. Defaults to ["sentinel-2-l2a", "landsat-c2-l2"].
+        days_search_window (int, optional): Number of days before the sample to
+            include in the search. Defaults to 15.
+        meters_search_window (int, optional): Buffer in meters to add on each side of
+            the sample location when searching for imagery. Defaults to 50000.
     """
     logger.info(f"Querying satellite data for {sample_list.shape[0]:,} samples")
 
     # Iterate over samples (parallelize later)
     for sample in tqdm(sample_list.itertuples()):
-        download_sample_imagery(sample.date, sample.latitude, sample.longitude)
+        # Search planetary computer
+        all_returned_items = search_planetary_computer(
+            sample.date,
+            sample.latitude,
+            sample.longitude,
+            collections=collections,
+            days_search_window=days_search_window,
+            meters_search_window=meters_search_window,
+        )
 
-    pass
+        # Select items from results
+        selected_items = select_items(
+            sample.date, sample.latitude, sample.longitude, all_returned_items
+        )
+
+        # Save items
+        for item in selected_items:
+            save_item(item, save_dir, sample.Index)
