7ececeb9d7109414f4117109ad2bff1eb5825892
Katie Wetstone <klwetstone@gmail.com>
2023-07-31 09:44:58 -0400

    update tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -10,6 +10,7 @@ import pytest
 from typer.testing import CliRunner
 
 from cyano.cli import train_model, predict_model, app
+from cyano.config import ExperimentConfig
 from cyano.data.features import generate_features
 from cyano.models.cyano_model import CyanoModel
 
@@ -17,19 +18,14 @@ runner = CliRunner()
 
 ASSETS_DIR = Path(__file__).parent / "assets"
 
-BASIC_CONFIG = {
-    "num_threads": 4,
-    "pc_collections": ["sentinel-2-l2a"],
-    "pc_days_search_window": 30,
-    "pc_meters_search_window": 1000,
+TRAIN_CONFIG = {
     "use_sentinel_bands": ["B02"],
     "image_feature_meter_window": 500,
     "satellite_features": ["B02_mean", "B02_min", "B02_max"],
-    "climate_features": [],
-    "elevation_features": [],
-    "metadata_features": [],
-    "lgb_config": {"params": {}},
 }
+
+PREDICT_CONFIG = TRAIN_CONFIG.copy()
+PREDICT_CONFIG["model_save_dir"] = str(ASSETS_DIR / "trained_model")
 
 
 @pytest.fixture
@@ -39,23 +35,23 @@ def train_data() -> pd.DataFrame:
 
 def test_train_model(train_data: pd.DataFrame):
     with tempfile.TemporaryDirectory() as model_dir:
-        config = BASIC_CONFIG.copy()
-        config["model_dir"] = model_dir
+        config = TRAIN_CONFIG.copy()
+        config["model_save_dir"] = model_dir
 
         # Run train model and check that it returns a model
         trained_model = train_model(train_data, config, debug=True)
         assert type(trained_model) == CyanoModel
 
         # Check that model config is saved correctly
-        saved_config_path = Path(trained_model.config["model_dir"]) / "config.json"
+        saved_config_path = Path(trained_model.config.model_save_dir) / "run_config.json"
         assert saved_config_path.exists()
         with open(saved_config_path, "r") as fp:
             saved_config = json.load(fp)
         assert "satellite_features" in saved_config.keys()
-        assert "cache_mode" in saved_config.keys()
+        assert "cache_dir" in saved_config.keys()
 
         # Check that LGB Booster is saved correctly
-        saved_lgb_path = Path(trained_model.config["model_dir"]) / "lgb_model.txt"
+        saved_lgb_path = Path(trained_model.config.model_save_dir) / "lgb_model.txt"
         assert saved_lgb_path.exists()
         lgb_model = lgb.Booster(model_file=saved_lgb_path)
         assert type(lgb_model) == lgb.Booster
@@ -66,7 +62,7 @@ def test_predict_model(train_data: pd.DataFrame):
     # Run predict and check that it returns a dataframe
     with tempfile.TemporaryDirectory() as preds_dir:
         preds_path = Path(preds_dir) / "preds.csv"
-        preds = predict_model(train_data, preds_path, ASSETS_DIR / "trained_model", debug=True)
+        preds = predict_model(train_data, preds_path, PREDICT_CONFIG, debug=True)
         assert preds.shape[0] == train_data.shape[0]
 
         # Check saved preds
@@ -77,7 +73,7 @@ def test_predict_model(train_data: pd.DataFrame):
 
 def test_cli_train():
     # Load config
-    config_path = ASSETS_DIR / "cli_config.json"
+    config_path = ASSETS_DIR / "cli_train_config.json"
     with open(config_path, "r") as fp:
         config = json.load(fp)
 
@@ -89,14 +85,14 @@ def test_cli_train():
     assert result.exit_code == 0
 
     # Check that model config saved out
-    saved_config_path = Path(config["model_dir"]) / "config.json"
+    saved_config_path = Path(config["model_save_dir"]) / "run_config.json"
     assert saved_config_path.exists()
 
     # Check that LGB Booster saved out
-    saved_lgb_path = Path(config["model_dir"]) / "lgb_model.txt"
+    saved_lgb_path = Path(config["model_save_dir"]) / "lgb_model.txt"
     assert saved_lgb_path.exists()
 
-    shutil.rmtree(config["model_dir"])
+    shutil.rmtree(config["model_save_dir"])
 
 
 def test_cli_predict(train_data: pd.DataFrame):
@@ -107,7 +103,7 @@ def test_cli_predict(train_data: pd.DataFrame):
             [
                 "predict",
                 str(ASSETS_DIR / "train_data.csv"),
-                str(ASSETS_DIR / "trained_model"),
+                str(ASSETS_DIR / "cli_predict_config.json"),
                 str(preds_path),
                 "--debug",
             ],
@@ -120,10 +116,11 @@ def test_cli_predict(train_data: pd.DataFrame):
 
 
 def test_known_features(train_data: pd.DataFrame):
-    config = BASIC_CONFIG.copy()
+    config = PREDICT_CONFIG.copy()
 
     # Generate features based on saved imagery
     config["cache_dir"] = str(ASSETS_DIR / "feature_cache")
+    config = ExperimentConfig(**config)
     features = generate_features(train_data.set_index("uid"), config)
 
     # Check that generated stats match known imagery stats
