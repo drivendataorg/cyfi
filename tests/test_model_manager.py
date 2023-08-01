eabb79dd766ba40ae316698bbfaa11ef6f49a1db
Emily Miller <ejm714@gmail.com>
2023-07-31 19:54:31 -0700

    make predict pass

--- tests/test_model_manager.py
+++ tests/test_model_manager.py
@@ -10,7 +10,6 @@ from cyano.models.cyano_model import CyanoModel
 
 
 def test_train_model(train_config, train_data):
-    import pdb; pdb.set_trace()
     # Run train model and check that it returns a model
     trained_model = train_model(train_data, train_config)
     assert type(trained_model) == CyanoModel
@@ -30,31 +29,13 @@ def test_train_model(train_config, train_data):
     assert lgb_model.feature_name() == train_config.features_config.satellite_features
 
 
-# def test_predict_model(tmp_path_factory, predict_data, predict_config):
-#     # Run predict and check that it returns a dataframe
-#     preds_path = tmp_path_factory.mktemp("test_predict") / "preds.csv"
-#     predict_config["preds_save_path"] = str(preds_path)
+def test_predict_model(predict_data, predict_config):
+    # Run predict and check that it returns a dataframe
+    preds = predict_model(predict_data, predict_config, debug=True)
+    assert preds.shape[0] == predict_data.shape[0]
 
-#     preds = predict_model(predict_data, predict_config, debug=True)
-#     assert preds.shape[0] == predict_data.shape[0]
+    # Check saved preds
+    assert Path(predict_config.save_path).exists()
+    saved_preds = pd.read_csv(predict_config.save_path)
+    assert saved_preds.shape[0] == predict_data.shape[0]
 
-#     # Check saved preds
-#     assert preds_path.exists()
-#     saved_preds = pd.read_csv(preds_path)
-#     assert saved_preds.shape[0] == predict_data.shape[0]
-
-
-
-
-# def test_known_features(train_data: pd.DataFrame):
-#     config = PREDICT_CONFIG.copy()
-
-#     # Generate features based on saved imagery
-#     config["cache_dir"] = str(ASSETS_DIR / "feature_cache")
-#     config = TrainConfig(**config)
-#     features = generate_features(train_data.set_index("uid"), config)
-
-#     # Check that generated stats match known imagery stats
-#     assert np.isclose(features.loc["ofhd", "B02_mean"], 161.532712)
-#     assert np.isclose(features.loc["ofhd", "B02_min"], 50)
-#     assert np.isclose(features.loc["ofhd", "B02_max"], 1182)
