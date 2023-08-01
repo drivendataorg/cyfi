d489c98b94a7cd61e6d5ba20801364d59e2b62e5
Emily Miller <ejm714@gmail.com>
2023-07-31 18:20:17 -0700

    update per config

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,8 @@ from pathlib import Path
 import pandas as pd
 import pytest
 
+from cyano.config import TrainConfig, PredictConfig, FeaturesConfig, ModelConfig
+
 ASSETS_DIR = Path(__file__).parent / "assets"
 
 
@@ -15,17 +17,16 @@ def predict_data() -> pd.DataFrame:
     return pd.read_csv(ASSETS_DIR / "train_data.csv")[["date", "latitude", "longitude"]]
 
 @pytest.fixture(scope="session")
-def train_config():
-    # TODO: should be pydantic model
-    return {
-        "use_sentinel_bands": ["B02"],
-        "image_feature_meter_window": 500,
-        "satellite_features": ["B02_mean", "B02_min", "B02_max"],
-    }
+def train_config(tmp_path_factory):
+    return TrainConfig(
+        features_config=FeaturesConfig(
+            use_sentinel_bands=["B02"],
+            image_feature_meter_window=500,
+            satellite_features=["B02_mean", "B02_min", "B02_max"],
+        ),
+        model_config=ModelConfig(save_dir=str(tmp_path_factory.mktemp("model_dir")))
+    )
 
 @pytest.fixture(scope="session")
 def predict_config(train_config):
-    # TODO: shouldn't include all the train config params
-    config = train_config.copy()
-    config["cyano_model_config"] = {"trained_model_dir": str(ASSETS_DIR / "trained_model")}
-    return config
+    return PredictConfig(trained_model_dir=str(ASSETS_DIR / "trained_model"))
