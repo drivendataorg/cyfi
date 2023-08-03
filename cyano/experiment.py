d762fc32532821aee3e1dcf2d81f87e413172cd4
Emily Miller <ejm714@gmail.com>
2023-08-02 19:07:00 -0700

    add experiment abstraction

---
+++ cyano/experiment.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import yaml
+
+from pydantic import BaseModel
+
+from cyano.config import FeaturesConfig, ModelTrainingConfig
+from cyano.pipeline import CyanoModelPipeline
+
+
+class ExperimentConfig(BaseModel):
+    features_config: FeaturesConfig = FeaturesConfig()
+    model_training_config: ModelTrainingConfig = ModelTrainingConfig()
+    train_csv: Path
+    predict_csv: Path
+    cache_dir: Path = None
+    save_dir: Path = None
+
+
+    def run_experiment(self):
+        pipeline = CyanoModelPipeline(
+            features_config=self.features_config,
+            model_training_config=self.model_training_config,
+            cache_dir=self.cache_dir,
+        )
+        pipeline.run_training(
+            train_csv=self.train_csv, save_path=self.save_dir / "model.zip"
+        )
+
+        with open(f"{self.save_dir}/config_artifact.yaml", "w") as fp:
+            yaml.dump(self.model_dump(), fp)
+
+        pipeline.run_prediction(predict_csv=self.predict_csv, preds_path=self.save_dir / "preds.csv")
+
