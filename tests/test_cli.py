fa4c452fa6ef1090667386144715a5e990e60a30
Emily Miller <ejm714@gmail.com>
2023-08-03 15:46:24 -0700

    passing cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,46 +5,47 @@ from pathlib import Path
 from typer.testing import CliRunner
 
 from cyano.cli import app
+from cyano.experiment import ExperimentConfig
 
 ASSETS_DIR = Path(__file__).parent / "assets"
 
 runner = CliRunner()
 
 
-def test_cli_train(tmp_path, train_data_path, train_config):
-    # Write out config to tmp dir
-    config_path = tmp_path / "train_config.yaml"
-    with open(config_path, "w") as fp:
-        yaml.dump(train_config.model_dump(), fp)
-
+def test_cli_experiment(experiment_config_path):
     # Run CLI command
     result = runner.invoke(
         app,
-        ["train", str(train_data_path), str(config_path)],
+        ["experiment", str(experiment_config_path)],
     )
     assert result.exit_code == 0
 
-    # Check that experiment config saved out
-    assert (Path(train_config.tree_model_config.save_dir) / "config.yaml").exists()
+    with experiment_config_path.open("r") as f:
+        config = ExperimentConfig(**yaml.safe_load(f))
 
-    # Check that LGB Booster saved out
-    assert (Path(train_config.tree_model_config.save_dir) / "lgb_model.txt").exists()
+    # Check that artifact config, model zip, and predictions got saved out
+    assert (Path(config.save_dir) / "config_artifact.yaml").exists()
+    assert (Path(config.save_dir) / "model.zip").exists()
+    assert (Path(config.save_dir) / "preds.csv").exists()
 
 
-def test_cli_predict(tmp_path, predict_data_path, predict_data, predict_config):
-    # Write out config to tmp dir
-    config_path = tmp_path / "predict_config.yam;"
-    with open(config_path, "w") as fp:
-        yaml.dump(predict_config.model_dump(), fp)
+def test_cli_predict(tmp_path, predict_data_path, predict_data):
+    preds_path = tmp_path / "preds.csv"
 
     # Run CLI command
     result = runner.invoke(
         app,
-        ["predict", str(predict_data_path), str(config_path)],
+        [
+            "predict",
+            str(predict_data_path),
+            str(ASSETS_DIR / "experiment/model.zip"),
+            "--output-path",
+            str(preds_path),
+        ],
     )
     assert result.exit_code == 0
 
     # Check that preds saved out
-    assert Path(predict_config.preds_path).exists()
-    preds = pd.read_csv(predict_config.preds_path)
+    assert preds_path.exists()
+    preds = pd.read_csv(preds_path)
     assert len(preds) == len(predict_data)
