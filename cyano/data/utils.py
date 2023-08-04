6e718aa3d770bac38e70e867f5ec38ec39bdc7f9
Emily Miller <ejm714@gmail.com>
2023-08-04 15:15:52 -0700

    make hash consistent

--- cyano/data/utils.py
+++ cyano/data/utils.py
@@ -1,16 +1,11 @@
-from faker import Faker
-import numpy as np
+import hashlib
+
 import pandas as pd
 
-RANDOM_SEED = 40
-rng = np.random.RandomState(RANDOM_SEED)
-fake = Faker()
-Faker.seed(RANDOM_SEED)
 
-
-def add_unique_identifier(df: pd.DataFrame, id_len: int = 4) -> pd.DataFrame:
-    """Given a dataframe, create a unique identifier for each row
-    and set as the index
+def add_unique_identifier(df: pd.DataFrame, id_len: int = 10) -> pd.DataFrame:
+    """Given a dataframe with the columns []"latitude", "longitude", "date"],
+    create a unique identifier for each row and set as the index
 
     Args:
         df (pd.DataFrame): Dataframe
@@ -19,21 +14,14 @@ def add_unique_identifier(df: pd.DataFrame, id_len: int = 4) -> pd.DataFrame:
         pd.DataFrame: Dataframe with unique identifiers as the index
     """
     df = df.copy()
-    redundancy = 3
-    ids = [
-        fake.unique.pystr(min_chars=id_len, max_chars=id_len).lower()
-        for _ in range(len(df) * redundancy)
-    ]
-    unique_ids = set(ids)
-    while len(unique_ids) < len(df):
-        redundancy += 1
-        ids = [
-            fake.unique.pystr(min_chars=id_len, max_chars=id_len).lower()
-            for _ in range(len(df) * redundancy)
-        ]
-        unique_ids = set(ids)
+    m = hashlib.md5()
+    uids = []
 
-    ids = list(unique_ids)[: len(df)]
-    df["uid"] = ids
+    # create UID based on lat/lon and date
+    for row in df.itertuples():
+        for s in (row.latitude, row.longitude, row.date):
+            m.update(str(s).encode())
+        uids.append(m.hexdigest()[:id_len])
 
+    df["uid"] = uids
     return df.set_index("uid")
