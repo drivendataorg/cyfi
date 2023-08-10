cdb71a49dbba7e51d6baabf7f1f141a9cb4a3de4
Katie Wetstone <klwetstone@gmail.com>
2023-08-10 17:15:24 -0500

    drop rows missing satellite data

--- cyano/data/features.py
+++ cyano/data/features.py
@@ -12,23 +12,76 @@ from cyano.config import FeaturesConfig
 # Create a dictionary mapping feature names to feature generator
 # functions, which take a dictionary of band arrays as input
 SATELLITE_FEATURE_CALCULATORS = {
-    "ndvi_b04": lambda x: (x["B08"].mean() - x["B04"].mean())
+    "NDVI_B04": lambda x: (x["B08"].mean() - x["B04"].mean())
     / (x["B08"].mean() + x["B04"].mean() + 1),
-    "ndvi_b05": lambda x: (x["B08"].mean() - x["B05"].mean())
+    "NDVI_B05": lambda x: (x["B08"].mean() - x["B05"].mean())
     / (x["B08"].mean() + x["B05"].mean() + 1),
-    "ndvi_b06": lambda x: (x["B08"].mean() - x["B06"].mean())
+    "NDVI_B06": lambda x: (x["B08"].mean() - x["B06"].mean())
     / (x["B08"].mean() + x["B06"].mean() + 1),
+    "NDVI_B07": lambda x: (x["B08"].mean() - x["B07"].mean())
+    / (x["B08"].mean() + x["B07"].mean() + 1),
     "blue_red_ratio": lambda x: x["B02"].mean() / x["B04"].mean(),
     "blue_green_ratio": lambda x: x["B02"].mean() / x["B03"].mean(),
+    "AOT_mean": lambda x: x["AOT"].mean(),
+    "AOT_min": lambda x: x["AOT"].min(),
+    "AOT_max": lambda x: x["AOT"].max(),
+    "AOT_range": lambda x: x["AOT"].max() - x["AOT"].min(),
+    "B01_mean": lambda x: x["B01"].mean(),
+    "B01_min": lambda x: x["B01"].min(),
+    "B01_max": lambda x: x["B01"].max(),
+    "B01_range": lambda x: x["B01"].max() - x["B01"].min(),
     "B02_mean": lambda x: x["B02"].mean(),
     "B02_min": lambda x: x["B02"].min(),
     "B02_max": lambda x: x["B02"].max(),
+    "B02_range": lambda x: x["B02"].max() - x["B02"].min(),
     "B03_mean": lambda x: x["B03"].mean(),
     "B03_min": lambda x: x["B03"].min(),
     "B03_max": lambda x: x["B03"].max(),
+    "B03_range": lambda x: x["B03"].max() - x["B03"].min(),
     "B04_mean": lambda x: x["B04"].mean(),
     "B04_min": lambda x: x["B04"].min(),
     "B04_max": lambda x: x["B04"].max(),
+    "B04_range": lambda x: x["B04"].max() - x["B04"].min(),
+    "B05_mean": lambda x: x["B05"].mean(),
+    "B05_min": lambda x: x["B05"].min(),
+    "B05_max": lambda x: x["B05"].max(),
+    "B05_range": lambda x: x["B05"].max() - x["B05"].min(),
+    "B06_mean": lambda x: x["B06"].mean(),
+    "B06_min": lambda x: x["B06"].min(),
+    "B06_max": lambda x: x["B06"].max(),
+    "B06_range": lambda x: x["B06"].max() - x["B06"].min(),
+    "B07_mean": lambda x: x["B07"].mean(),
+    "B07_min": lambda x: x["B07"].min(),
+    "B07_max": lambda x: x["B07"].max(),
+    "B07_range": lambda x: x["B07"].max() - x["B07"].min(),
+    "B08_mean": lambda x: x["B08"].mean(),
+    "B08_min": lambda x: x["B08"].min(),
+    "B08_max": lambda x: x["B08"].max(),
+    "B08_range": lambda x: x["B08"].max() - x["B08"].min(),
+    "B09_mean": lambda x: x["B09"].mean(),
+    "B09_min": lambda x: x["B09"].min(),
+    "B09_max": lambda x: x["B09"].max(),
+    "B09_range": lambda x: x["B09"].max() - x["B09"].min(),
+    "B11_mean": lambda x: x["B11"].mean(),
+    "B11_min": lambda x: x["B11"].min(),
+    "B11_max": lambda x: x["B11"].max(),
+    "B11_range": lambda x: x["B11"].max() - x["B11"].min(),
+    "B12_mean": lambda x: x["B12"].mean(),
+    "B12_min": lambda x: x["B12"].min(),
+    "B12_max": lambda x: x["B12"].max(),
+    "B12_range": lambda x: x["B12"].max() - x["B12"].min(),
+    "B8A_mean": lambda x: x["B8A"].mean(),
+    "B8A_min": lambda x: x["B8A"].min(),
+    "B8A_max": lambda x: x["B8A"].max(),
+    "B8A_range": lambda x: x["B8A"].max() - x["B8A"].min(),
+    "SCL_mean": lambda x: x["SCL"].mean(),
+    "SCL_min": lambda x: x["SCL"].min(),
+    "SCL_max": lambda x: x["SCL"].max(),
+    "SCL_range": lambda x: x["SCL"].max() - x["SCL"].min(),
+    "WVP_mean": lambda x: x["WVP"].mean(),
+    "WVP_min": lambda x: x["WVP"].min(),
+    "WVP_max": lambda x: x["WVP"].max(),
+    "WVP_range": lambda x: x["WVP"].max() - x["WVP"].min(),
 }
 
 
@@ -91,13 +144,6 @@ def generate_satellite_features(
 
     satellite_features = pd.concat(satellite_features, axis=1).T
 
-    # For now, fill missing imagery values with the average over all samples
-    logger.info(
-        f"Filling missing satellite values for {satellite_features.isna().any(axis=1).sum()} samples"
-    )
-    for col in config.satellite_image_features:
-        satellite_features[col] = satellite_features[col].fillna(satellite_features[col].mean())
-
     # Add in satellite meta features
     satellite_features = satellite_features.merge(
         satellite_meta[["item_id", "sample_id"] + config.satellite_meta_features],
@@ -112,7 +158,7 @@ def generate_satellite_features(
             "There are repeat sample / item combinations in the satellite features dataframe"
         )
 
-    return satellite_features.set_index("sample_id").drop(columns=["item_id"])
+    return satellite_features.set_index("sample_id").drop(columns=["item_id"]).astype(float)
 
 
 def generate_climate_features(
@@ -206,7 +252,9 @@ def generate_features(
 
     Returns:
         pd.DataFrame: Dataframe where the index is uid and there is one
-            column for each feature
+            column for each feature. Each row is a unique combination of
+            sample and pystac item. Only samples that have valid satellite
+            imagery are included in the features
     """
     # Generate satellite features
     # May be >1 row per sample, only includes samples with imagery
@@ -214,35 +262,31 @@ def generate_features(
     logger.info(
         f"Generated {satellite_features.shape[1]} satellite features. {satellite_features.index.nunique():,} samples, {satellite_features.shape[0]:,} item / sample combinations."
     )
+    feature_uids = satellite_features.index
 
     # Generate non-satellite features. Each has only one row per sample
-    uids = samples.index
+    # Only include samples for which we have satellite features
+    unique_feature_uids = satellite_features.index.unique
     non_satellite_features = []
     if config.climate_features:
-        climate_features = generate_climate_features(uids, config, cache_dir)
-        non_satellite_features.append(climate_features.loc[uids])
+        climate_features = generate_climate_features(unique_feature_uids, config, cache_dir)
+        non_satellite_features.append(climate_features.loc[feature_uids])
         logger.info(f"Generated {satellite_features.shape[0]} climate features")
 
     if config.elevation_features:
-        elevation_features = generate_elevation_features(uids, config, cache_dir)
-        non_satellite_features.append(elevation_features.loc[uids])
+        elevation_features = generate_elevation_features(unique_feature_uids, config, cache_dir)
+        non_satellite_features.append(elevation_features.loc[feature_uids])
         logger.info(f"Generated {satellite_features.shape[0]} elevation features")
 
     if config.metadata_features:
         metadata_features = generate_metadata_features(samples, config)
-        non_satellite_features.append(metadata_features.loc[uids])
+        non_satellite_features.append(metadata_features.loc[feature_uids])
 
         logger.info(f"Generated {satellite_features.shape[0]} metadata features")
     non_satellite_features = pd.concat(non_satellite_features, axis=1)
 
     # Merge satellite and non-satellite features
-    features = non_satellite_features.merge(
-        satellite_features, how="outer", left_index=True, right_index=True, validate="1:m"
-    )
-    if set(features.index.unique()) != set(samples.index.unique()):
-        raise ValueError(
-            "The list of samples in the features dataframe does not match the original list of samples"
-        )
+    features = pd.concat([satellite_features, non_satellite_features], axis=1)
 
     all_feature_cols = (
         config.satellite_image_features
