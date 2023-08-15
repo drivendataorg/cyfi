ac08bf11f0a2032c754fb2276f4906b114d401a1
Katie Wetstone <klwetstone@gmail.com>
2023-08-15 11:38:58 -0400

    fix evaluate cli

--- cyano/cli.py
+++ cyano/cli.py
@@ -1,6 +1,8 @@
 import yaml
+from zipfile import ZipFile
 
 from cloudpathlib import AnyPath
+import lightgbm as lgb
 from pathlib import Path
 import typer
 
@@ -44,14 +46,13 @@ def predict(
 
 @app.command()
 def evaluate(
-    y_pred_csv: Path = typer.Argument(
-        exists=True,
-        help="Path to a csv of samples with columns for date, longitude, latitude, and severity",
+    y_pred_csv: str = typer.Argument(
+        help="Path to a csv of samples with columns for date, longitude, latitude, and predicted severity",
     ),
-    y_true_csv: Path = typer.Argument(
-        exists=True,
-        help="Path to a csv of samples with columns for date, longitude, latitude, and severity, with optional metadata columns",
+    y_true_csv: str = typer.Argument(
+        help="Path to a csv of samples with columns for date, longitude, latitude, and actual severity, with optional metadata columns",
     ),
+    model_path: Path = typer.Argument(exists=True, help="Path to trained LGB model"),
     save_dir: Path = typer.Option(
         default=Path.cwd() / "metrics", help="Folder in which to save out metrics and plots."
     ),
@@ -59,8 +60,12 @@ def evaluate(
     y_pred_csv = AnyPath(y_pred_csv)
     y_true_csv = AnyPath(y_true_csv)
 
+    # Load model from model zipfile
+    archive = ZipFile(model_path, "r")
+    model = lgb.Booster(model_str=archive.read("lgb_model.txt").decode())
+
     EvaluatePreds(
-        y_pred_csv=y_pred_csv, y_true_csv=y_true_csv, save_dir=save_dir
+        y_pred_csv=y_pred_csv, y_true_csv=y_true_csv, save_dir=save_dir, model=model
     ).calculate_all_and_save()
 
 
