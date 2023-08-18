d64d1f7f30689446d44fe96499048dfcf739a023
Katie Wetstone <klwetstone@gmail.com>
2023-08-18 11:54:15 -0400

    update refactored climate data

--- cyano/data/climate_data.py
+++ cyano/data/climate_data.py
@@ -1,6 +1,5 @@
 import functools
 import os
-from typing import List
 
 from cloudpathlib import AnyPath
 from herbie import FastHerbie
@@ -13,6 +12,12 @@ from tqdm.contrib.concurrent import process_map
 from cyano.config import FeaturesConfig
 
 TRY_GRID_BUFFERS = np.arange(start=0.01, stop=0.1, step=0.01)
+
+# Save consistent example dates to initialize FastHerbie
+# Include a range to ensure we get grid mapping results for all samples
+HERBIE_EXAMPLE_DATES = [
+    d for d in pd.date_range(start="2014-09-01", end="2022-01-01", freq="6m", inclusive="both")
+]
 
 
 def path_to_climate_data(sample_id, var_name, level, cache_dir):
@@ -70,9 +75,7 @@ def get_location_grid(
     return None
 
 
-def query_grid_mapping(
-    samples: pd.DataFrame, cache_dir, example_dates: List = None
-) -> pd.DataFrame:
+def query_grid_mapping(samples: pd.DataFrame, cache_dir) -> pd.DataFrame:
     """Returns a dataframe with one row for each combination of sample ID and
     HRRR data grid location (x,y)
 
@@ -86,14 +89,7 @@ def query_grid_mapping(
     NUM_PROCESSES = int(os.getenv("CY_NUM_PROCESSES", 4))
 
     ## Initialize FastHerbie with example dates
-    if example_dates is None:
-        example_dates = pd.concat(
-            [
-                samples.groupby(samples.date.dt.year).head(1),
-                samples.groupby(samples.date.dt.year).tail(1),
-            ]
-        ).date.tolist()
-    FH = FastHerbie(example_dates, model="hrrr", fxx=[0], verbose=False)
+    FH = FastHerbie(HERBIE_EXAMPLE_DATES, model="hrrr", fxx=[0], verbose=False)
     # Read into an xarray and convert to dataframe
     herbie_xarray = FH.xarray("TMP:2 m", remove_grib=False)
     xarrays_df = herbie_xarray.to_dataframe().reset_index()
@@ -105,7 +101,7 @@ def query_grid_mapping(
         f"Iterating over {locations.shape[0]:,} locations to get HRRR grid indices with {NUM_PROCESSES} workers"
     )
     results = []
-    for chunk in np.array_split(locations, 10):
+    for chunk in np.array_split(locations, max(1, len(locations) // 1000)):
         chunk_results = process_map(
             functools.partial(get_location_grid, xarrays_df=xarrays_df),
             chunk.latitude,
@@ -139,8 +135,6 @@ def query_grid_mapping(
 
 
 def load_hrrr_grid(samples: pd.DataFrame, cache_dir):
-    samples = process_samples_for_hrrr(samples)
-
     ## Get mapping of sample locations to grid indices in HRRR
     grid_save_path = cache_dir / "hrrr_sample_grid_mapping.csv"
 
@@ -153,24 +147,17 @@ def load_hrrr_grid(samples: pd.DataFrame, cache_dir):
         missing_samples = samples[~samples.index.isin(sample_grid_map.index)]
         if len(missing_samples) == 0:
             # Do not need to query for any additional samples
+            sample_grid_map["date"] = pd.to_datetime(sample_grid_map.date)
             return sample_grid_map.loc[samples.index]
 
         logger.info(f"Generating grid for remaining {missing_samples.shape[0]:,} samples")
-        herbie_example_dates = pd.concat(
-            [
-                samples.groupby(samples.date.dt.year).head(1),
-                samples.groupby(samples.date.dt.year).tail(1),
-            ]
-        ).date.tolist()
-        new_sample_grid_map = query_grid_mapping(
-            missing_samples, cache_dir, example_dates=herbie_example_dates
-        )
-        sample_grid_map = pd.concat([sample_grid_map, new_sample_grid_map])
+        new_sample_grid_map = query_grid_mapping(missing_samples, cache_dir)
+        sample_grid_map = pd.concat([sample_grid_map, new_sample_grid_map]).drop_duplicates()
 
     # Otherwise generate grid for all samples
     else:
         logger.info(f"Generating HRRR grid for {samples.shape[0]:,} samples")
-        sample_grid_map = query_grid_mapping(samples, cache_dir)
+        sample_grid_map = query_grid_mapping(samples, cache_dir).drop_duplicates()
 
     logger.info(
         f"Saving updated grid mapping with {sample_grid_map.index.nunique():,} samples to {grid_save_path}"
@@ -208,22 +195,10 @@ def download_climate_for_date(
     var_name: str,
     level: str,
 ):
-    # First, check if samples for given date are already downloaded
-    date_sample_paths = (
-        sample_grid_map[sample_grid_map.date == date].reset_index().sample_id.drop_duplicates()
-    )
-    date_sample_paths = date_sample_paths.apply(
-        functools.partial(
-            path_to_climate_data, var_name=var_name, level=level, cache_dir=cache_dir
-        )
-    )
-    if date_sample_paths.path.exists().all():
-        return
-
-    # Otherwise query for climate data
+    # Query for climate data
     try:
         query_timestamps = timestamps_per_date[
-            timestamps_per_date.sample_date == date
+            timestamps_per_date.sample_date.dt.date == date.date()
         ].query_timestamp.tolist()
         FH = FastHerbie(query_timestamps, model="hrrr", fxx=[0], verbose=False)
         xarray_data = FH.xarray(f"{var_name}:{level}", remove_grib=False).drop_vars(
@@ -233,7 +208,9 @@ def download_climate_for_date(
 
         # Filter to grid locations that we need for the given date
         xarray_df = xarray_df.merge(
-            sample_grid_map[sample_grid_map.date == date].reset_index(), on=["x", "y"], how="inner"
+            sample_grid_map[sample_grid_map.date.dt.date == date.date()].reset_index(),
+            on=["x", "y"],
+            how="inner",
         )
     except Exception as e:
         logger.warning(f"{type(e)}: {e} for {date}")
@@ -259,22 +236,46 @@ def download_climate_data(samples: pd.DataFrame, config: FeaturesConfig, cache_d
     # Determine the number of processes to use when parallelizing
     NUM_PROCESSES = int(os.getenv("CY_NUM_PROCESSES", 4))
 
-    logger.info(f"Querying climate data for {samples.shape[0]:,} samples")
+    ## Check which samples are missing expected climate files
     samples = process_samples_for_hrrr(samples)
+    for var_name, level in config.use_climate_sources:
+        newcol = f"exists_{var_name}_{level}"
+        samples[newcol] = samples.index.map(
+            functools.partial(
+                path_to_climate_data, var_name=var_name, level=level, cache_dir=cache_dir
+            )
+        )
+        samples[newcol] = samples[newcol].path.exists()
+    missing_any_climate_files_mask = ~samples.filter(regex="exists_").all(axis=1)
+
+    if missing_any_climate_files_mask.sum() == 0:
+        logger.success(
+            f"All required climte files have already been downloaded for sources {config.use_climate_sources}"
+        )
+        return
+
+    samples = samples[missing_any_climate_files_mask].copy()
+    logger.info(
+        f"Getting climate data for {samples.shape[0]:,} samples for sources {config.use_climate_sources}"
+    )
 
     ## Get mapping of sample locations to grid indices in HRRR
     sample_grid_map = load_hrrr_grid(samples, cache_dir)
-    logger.info(
-        f"Loaded HRRR grid mapping for {sample_grid_map.index.nunique():,} samples ({sample_grid_map.shape[0]:,} rows total)"
-    )
 
-    ## Download climate data for each date
-    dates = samples.date.unique()
-    timestamps_per_date = get_timestamps_per_date(dates)
-    for climate_source in config.use_climate_sources:
-        var_name, level = climate_source
-        logger.info(f"Downloading {var_name} at {level} for {len(dates):,} dates")
-        process_map(
+    ## Iterate over required climate data sources
+    for var_name, level in config.use_climate_sources:
+        # Create source / level directory
+        path_to_climate_data("sample_id", var_name, level, cache_dir).parent.mkdir(
+            exist_ok=True, parents=True
+        )
+
+        # Check which samples are missing files for this climate source
+        missing_files_mask = samples[f"exists_{var_name}_{level}"] != True
+        missing_dates = samples[missing_files_mask].date.unique()
+        timestamps_per_date = get_timestamps_per_date(missing_dates)
+        logger.info(f"Downloading {var_name} at {level} for {len(missing_dates):,} date(s)")
+        # Download climate data for each date
+        _ = process_map(
             functools.partial(
                 download_climate_for_date,
                 timestamps_per_date=timestamps_per_date,
@@ -283,8 +284,8 @@ def download_climate_data(samples: pd.DataFrame, config: FeaturesConfig, cache_d
                 var_name=var_name,
                 level=level,
             ),
-            dates,
+            missing_dates,
             max_workers=NUM_PROCESSES,
             chunksize=1,
-            total=len(dates),
+            total=len(missing_dates),
         )
