9ba4a640d2fb20021905bb1e0b0b51eeca75e62f
Katie Wetstone <klwetstone@gmail.com>
2023-08-22 14:00:59 -0400

    test that samples with imagery have preds

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,10 +1,13 @@
 import yaml
 
 import pandas as pd
+from pandas_path import path  # noqa
 from pathlib import Path
 from typer.testing import CliRunner
+from zipfile import ZipFile
 
 from cyano.cli import app
+from cyano.config import FeaturesConfig
 from cyano.experiment.experiment import ExperimentConfig
 
 ASSETS_DIR = Path(__file__).parent / "assets"
@@ -30,7 +33,9 @@ def test_cli_experiment(experiment_config_path):
 
 
 def test_cli_predict(tmp_path, predict_data_path, predict_data):
+    model_path = ASSETS_DIR / "experiment/model.zip"
     preds_path = tmp_path / "preds.csv"
+    cache_dir = tmp_path / "cache"
 
     # Run CLI command
     result = runner.invoke(
@@ -38,9 +43,11 @@ def test_cli_predict(tmp_path, predict_data_path, predict_data):
         [
             "predict",
             str(predict_data_path),
-            str(ASSETS_DIR / "experiment/model.zip"),
+            str(model_path),
             "--output-path",
             str(preds_path),
+            "--cache-dir",
+            str(cache_dir),
         ],
     )
     assert result.exit_code == 0
@@ -49,3 +56,14 @@ def test_cli_predict(tmp_path, predict_data_path, predict_data):
     assert preds_path.exists()
     preds = pd.read_csv(preds_path)
     assert (preds.index == predict_data.index).all()
+
+    # Load features config to get sentinel meter buffer
+    archive = ZipFile(model_path, "r")
+    features_config = FeaturesConfig(**yaml.safe_load(archive.read("config.yaml")))
+
+    # Determine which samples have satellite imagery
+    preds["sat_dir"] = (
+        cache_dir / f"sentinel_{features_config.image_feature_meter_window}" / preds.sample_id.path
+    )
+    # Make sure those samples have predictions
+    assert preds[preds.sat_dir.path.exists()].severity.notna().any()
