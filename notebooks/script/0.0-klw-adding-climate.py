c3facbb8ff60f96c1ef4f443870e90ebf99876bc
Katie Wetstone <klwetstone@gmail.com>
2023-08-24 17:48:24 -0400

    delete extra nb cells

--- notebooks/script/0.0-klw-adding-climate.py
+++ notebooks/script/0.0-klw-adding-climate.py
@@ -3,7 +3,7 @@
 
 # Look at the results of adding climate data. See the impact on performance for samples with satellite imagery vs. samples with only climate data
 
-get_ipython().run_line_magic('load_ext', 'lab_black')
+# %load_ext lab_black
 get_ipython().run_line_magic('load_ext', 'autoreload')
 get_ipython().run_line_magic('autoreload', '2')
 
@@ -23,6 +23,8 @@ from cyano.evaluate import (
     generate_and_plot_crosstab,
     generate_actual_density_boxplot,
     generate_regional_barplot,
+    generate_density_scatterplot,
+    generate_density_kdeplot
 )
 
 
@@ -39,15 +41,14 @@ true = pd.read_csv(true_path, index_col=0)
 true.shape
 
 
-# # With metadata features
-# 
-# With metadata feature for `rounded_longitude`
-
 # load predictions
 preds = pd.read_csv(
     s3_dir / "results/third_sentinel_and_climate/preds.csv", index_col=0
 )
 preds.shape
+
+
+preds.head()
 
 
 preds.isna().sum()
@@ -126,23 +127,29 @@ evals["without_satellite"] = EvaluatePreds(
 )
 
 
-# ### Evaluate
+# ## Evaluate
+
+# ### Severity level
 
 fig, axes = plt.subplots(1, 2, figsize=(9, 4), sharex=True, sharey=True)
 for i, key in enumerate(evals.keys()):
-    generate_and_plot_crosstab(evals[key].y_true, evals[key].y_pred, ax=axes[i])
+    generate_and_plot_crosstab(evals[key].y_true_df.severity, evals[key].y_pred_df.severity, ax=axes[i])
     axes[i].set_title(key)
 
 
 fig, axes = plt.subplots(1, 2, figsize=(9, 4), sharex=True, sharey=True)
 for i, key in enumerate(evals.keys()):
     generate_actual_density_boxplot(
-        evals[key].metadata.density_cells_per_ml, evals[key].y_pred, ax=axes[i]
+        evals[key].y_true_df.density_cells_per_ml, evals[key].y_pred_df.severity, ax=axes[i]
     )
     axes[i].set_title(key)
 
 
-results = {key: evalpreds.calculate_metrics() for key, evalpreds in evals.items()}
+severity_results = {key: evalpreds.calculate_severity_metrics(
+    y_true=evalpreds.y_true_df.severity,
+    y_pred=evalpreds.y_pred_df.severity,
+    region=evalpreds.region
+) for key, evalpreds in evals.items()}
 
 
 fig, axes = plt.subplots(1, 2, figsize=(8, 4))
@@ -153,7 +160,7 @@ for i, metric in enumerate(
         "regional_rmse",
     ]
 ):
-    regional_scores = pd.DataFrame({key: res[metric] for key, res in results.items()})
+    regional_scores = pd.DataFrame({key: res[metric] for key, res in severity_results.items()})
     regional_scores.plot(kind="bar", ax=axes[i])
     axes[i].set_title(metric)
 plt.show()
@@ -163,7 +170,7 @@ plt.show()
 regional_scores
 
 
-pd.DataFrame(results).loc[
+pd.DataFrame(severity_results).loc[
     [
         "overall_rmse",
         "overall_mae",
@@ -173,11 +180,23 @@ pd.DataFrame(results).loc[
 ]
 
 
-# **Takeaway**
-# 
-# The difference in performance between samples with and without satellite imagery is not huge. Samples without satellite imagery have noticeably higher RMSE in the northeast and a small amount higher in the midwest and south, but actually lower RMSE in the west.
-# 
-# The difference in performance is much greater when looking at MAE and MAPE than when looking at RMSE.
+# ### Log(density)
+
+fig, axes = plt.subplots(1, 2, figsize=(9, 4), sharex=True, sharey=True)
+for i, key in enumerate(evals.keys()):
+    generate_density_scatterplot(
+        evals[key].y_true_df.log_density, evals[key].y_pred_df.log_density, ax=axes[i]
+    )
+    axes[i].set_title(key)
+
+
+for i, key in enumerate(evals.keys()):
+    generate_density_kdeplot(
+        evals[key].y_true_df.log_density, evals[key].y_pred_df.log_density
+    )
+
+
+# ### Feature importances
 
 feature_importance = pd.read_csv(
     s3_dir / "results/third_sentinel_and_climate/metrics/feature_importance.csv",
@@ -191,10 +210,6 @@ feature_importance.sort_values(by="importance_gain", ascending=False).head(10)
 # what are the top features by importance split?
 feature_importance.sort_values(by="importance_split", ascending=False).head(10)
 
-
-# **Takeaways**
-# 
-# All of our climate features are in the top most important features. Even though performance is somewhat lower on data with no satellite imagery, this implies that the model is still able to glean useful information with climate data alone.
 
 # ### Which new samples do we get features for?
 # 
@@ -222,133 +237,6 @@ new_coverage.date.dt.year.value_counts().sort_index()
 # 
 # Most of the samples climate data enables us to cover are on the older side. There are almost none post-2017 that climate covers but satellite does not.
 
-# # Without metadata feature
-# 
-# Look into performance without the metadata feature of `rounded_longitude`
-
-preds = pd.read_csv(
-    s3_dir / "results/third_sentinel_and_climate_no_meta/preds.csv", index_col=0
-)
-preds.shape
-
-
-preds.isna().sum()
-
-
-# ### Save out subsets
-# 
-# So that we can instantiate separate `EvaluatePreds` classes, save out two versions of the predictions -- one with only samples that have satellite imagery, one with only samples that *don't* have satellite imagery
-
-# Load test features to see which samples had satellite imagery
-test_features = pd.read_csv(
-    s3_dir / "results/third_sentinel_and_climate_no_meta/features_test.csv"
-)
-test_features.shape, test_features.sample_id.nunique()
-
-
-# how many have climate features?
-# checks out, same as experiment with metadata
-test_features[test_features.SPFH_max.notna()].sample_id.nunique()
-
-
-# same number have satellite features as well
-test_features["has_satellite"] = test_features.AOT_mean.notna()
-test_features[test_features.has_satellite].sample_id.nunique()
-
-
-preds_sat_path = tmp_save_dir / "preds_with_sat.csv"
-preds_no_sat_path = tmp_save_dir / "preds_no_sat.csv"
-
-# Save out subset with satellite imagery
-preds_sat = preds.loc[test_features[test_features.has_satellite].sample_id.unique()]
-print(preds_sat.shape)
-preds_sat.to_csv(preds_sat_path, index=True)
-
-# Save out subset without satellite imagery
-preds_no_sat = preds.loc[test_features[~test_features.has_satellite].sample_id]
-print(preds_no_sat.shape)
-preds_no_sat.to_csv(preds_no_sat_path, index=True)
-
-# Load model
-archive = ZipFile(s3_dir / "results/third_sentinel_and_climate_no_meta/model.zip", "r")
-model = lgb.Booster(model_str=archive.read("lgb_model.txt").decode())
-type(model)
-
-
-# ### Instantiate `EvaluatePreds` classes
-
-evals = {}
-evals["with_satellite"] = EvaluatePreds(
-    true_path, preds_sat_path, "tmp/eval_sat", model
-)
-
-
-evals["without_satellite"] = EvaluatePreds(
-    true_path, preds_no_sat_path, "tmp/eval_sat", model
-)
-
-
-# ### Evaluate
-
-fig, axes = plt.subplots(1, 2, figsize=(9, 4), sharex=True, sharey=True)
-for i, key in enumerate(evals.keys()):
-    generate_and_plot_crosstab(evals[key].y_true, evals[key].y_pred, ax=axes[i])
-    axes[i].set_title(key)
-
-
-fig, axes = plt.subplots(1, 2, figsize=(9, 4), sharex=True, sharey=True)
-for i, key in enumerate(evals.keys()):
-    generate_actual_density_boxplot(
-        evals[key].metadata.density_cells_per_ml, evals[key].y_pred, ax=axes[i]
-    )
-    axes[i].set_title(key)
-
-
-results = {key: evalpreds.calculate_metrics() for key, evalpreds in evals.items()}
-
-
-fig, axes = plt.subplots(1, 2, figsize=(8, 4))
-
-for i, metric in enumerate(
-    [
-        "regional_mae",
-        "regional_rmse",
-    ]
-):
-    regional_scores = pd.DataFrame({key: res[metric] for key, res in results.items()})
-    regional_scores.plot(kind="bar", ax=axes[i])
-    axes[i].set_title(metric)
-plt.show()
-
-
-# See RMSE in detail for regions
-regional_scores
-
-
-pd.DataFrame(results).loc[
-    [
-        "overall_rmse",
-        "overall_mae",
-        "overall_mape",
-        "region_averaged_rmse",
-    ]
-]
-
-
-feature_importance = pd.read_csv(
-    s3_dir
-    / "results/third_sentinel_and_climate_no_meta/metrics/feature_importance.csv",
-    index_col=0,
-)
-
-# what are the top features by importance gain?
-feature_importance.sort_values(by="importance_gain", ascending=False).head(10)
-
-
-# what are the top features by importance split?
-feature_importance.sort_values(by="importance_split", ascending=False).head(10)
-
-
 
 
 
@@ -357,95 +245,6 @@ feature_importance.sort_values(by="importance_split", ascending=False).head(10)
 
 # delete temporary dir
 shutil.rmtree(tmp_save_dir)
-
-
-
-
-
-# ***
-# 
-# # scrap
-
-from cyano.settings import REPO_ROOT
-from cyano.config import FeaturesConfig, ModelTrainingConfig
-from cyano.pipeline import CyanoModelPipeline
-from cyano.data.climate_data import download_climate_data
-from cyano.data.features import generate_features
-import numpy as np
-
-
-train = pd.read_csv(REPO_ROOT.parent / "tests/assets/train_data.csv")
-train.shape
-
-
-train = add_unique_identifier(train)
-
-
-train.head()
-
-
-eval = pd.read_csv(REPO_ROOT.parent / "tests/assets/evaluate_data.csv")
-eval = add_unique_identifier(eval)
-
-predict = pd.read_csv(REPO_ROOT.parent / "tests/assets/predict_data.csv")
-predict = add_unique_identifier(predict)
-
-
-meta = pd.read_csv(
-    AnyPath(
-        "s3://drivendata-competition-nasa-cyanobacteria/data/final/combined_final_release.csv"
-    )
-)
-meta = add_unique_identifier(meta)
-meta.head(2)
-
-
-
-
-
-predict
-
-
-pipe = CyanoModelPipeline(
-    features_config=FeaturesConfig(
-        use_sentinel_bands=["B02"],
-        image_feature_meter_window=500,
-        satellite_image_features=["B02_mean", "B02_min", "B02_max"],
-        climate_features=["TMP_min", "SPFH_mean"],
-        climate_variables=["TMP", "SPFH"],
-        climate_level="2 m above ground",
-    ),
-    model_training_config=ModelTrainingConfig(),
-    cache_dir=REPO_ROOT.parent / "tests/assets/feature_cache",
-)
-
-
-train_path = REPO_ROOT.parent / "tests/assets/train_data.csv"
-pipe._prep_train_data(train_path, False)
-
-
-download_climate_data(pipe.train_samples, pipe.features_config, pipe.cache_dir)
-
-
-pipe.train_samples
-
-
-sat_meta = pd.read_csv(REPO_ROOT.parent / "tests/assets/satellite_metadata.csv")
-
-
-fts = generate_features(
-    pipe.train_samples, sat_meta, pipe.features_config, pipe.cache_dir
-)
-fts
-
-
-fts[["TMP_min", "SPFH_mean"]].notna().all()
-
-
-np.isclose(fts.loc["9c601f226c2af07d570134127a7fda27", "SPFH_mean"], 0.01440739)
-
-
-fts.loc["9c601f226c2af07d570134127a7fda27", "SPFH_mean"]
 
 
 
