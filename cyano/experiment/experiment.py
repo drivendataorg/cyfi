6f4327c62a9dc3315a3f9dab4d621a878c3cb446
Katie Wetstone <klwetstone@gmail.com>
2023-08-25 09:01:52 -0400

    save features to experiment dir

--- cyano/experiment/experiment.py
+++ cyano/experiment/experiment.py
@@ -5,6 +5,7 @@ import yaml
 from cloudpathlib import AnyPath
 import git
 from loguru import logger
+import pandas as pd
 from pydantic import BaseModel, ConfigDict, field_serializer, field_validator
 
 from cyano.config import FeaturesConfig, ModelTrainingConfig
@@ -84,6 +85,11 @@ class ExperimentConfig(BaseModel):
         pipeline.run_prediction(
             predict_csv=self.predict_csv, preds_path=self.save_dir / "preds.csv", debug=self.debug
         )
+
+        # Copy train and test features to experiment dir
+        for split in ["train", "test"]:
+            df = pd.read_csv(pipeline.cache_dir / f"features_{split}.csv")
+            df.to_csv(self.save_dir / f"features_{split}.csv", index=False)
 
         if self.debug:
             logger.info("Evaluation is not run in debug mode")
