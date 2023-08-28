c8338cee324c5e54ade3243216f04b06a206b871
Katie Wetstone <klwetstone@gmail.com>
2023-08-28 11:34:28 -0400

    add features config docstring

--- cyano/config.py
+++ cyano/config.py
@@ -39,6 +39,37 @@ class FeaturesConfig(BaseModel):
     elevation_features: Optional[List] = []
     metadata_features: Optional[List] = ["rounded_longitude"]
     scl_filter: Optional[bool] = False
+    """Features configuration
+
+    Args:
+        pc_collections (Optional[List], optional): Collections within the planetary computer to
+            search for satellite imagery. Defaults to ["sentinel-2-l2a"].
+        pc_days_search_window (Optional[int], optional): Number of days before a given sample was 
+            collected to include when searching the planetary computer. Defaults to 30.
+        pc_meters_search_window (Optional[int], optional): Buffer in meters to add on each side
+            of a given sample's location when searching the planetary computer for satellite imagery.
+            Defaults to 1000.
+        use_sentinel_bands (Optional[List], optional): All Sentinel-2 bands that will be used when
+            generating satellite imagery featues. Defaults to ["B02", "B03", "B04"].
+        image_feature_meter_window (Optional[int], optional): Buffer in meters on each side of a 
+            given sample's location to use when creating the bounding box for the subset of a satellite
+            image that will be used to generate features. Defaults to 500.
+        n_sentinel_items (Optional[int], optional): Maximum number of Sentinel-2 items to use for each
+            sample. Defaults to 1.
+        satellite_image_features (Optional[List], optional): List of satellite imagery features to
+            include. Defaults to [ "B02_mean", "B02_min", "B02_max", "B03_mean", "B03_min", 
+            "B03_max", "B04_mean", ].
+        satellite_meta_features (Optional[List], optional): List of satellite metadata features to
+            include. Defaults to [].
+        climate_features (Optional[List], optional): List of climate features to include. 
+            Defaults to [].
+        elevation_features (Optional[List], optional): List of elevation features to include.
+            Defaults to [].
+        metadata_features (Optional[List], optional): List of metadata features to include. Defaults 
+            to ["rounded_longitude"].
+        scl_filter (Optional[bool], optional): Whether to filter satellite imagery during feature
+            generation to pixels labeled as water by Sentinel-2's SCL band. Defaults to False.
+    """
 
 
 class ModelTrainingConfig(BaseModel):
