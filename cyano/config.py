fd845edc790ce585b1cbcd4b7ee0d42918db3d78
Katie Wetstone <klwetstone@gmail.com>
2023-08-30 09:41:44 -0400

    tweak docstrings

--- cyano/config.py
+++ cyano/config.py
@@ -54,24 +54,25 @@ class FeaturesConfig(BaseModel):
 
     Args:
         pc_days_search_window (Optional[int], optional): Number of days before a given sample was
-            collected to include when searching the planetary computer. Defaults to 30.
+            collected to include when searching the planetary computer for satellite imagery.
+            Defaults to 30.
         pc_meters_search_window (Optional[int], optional): Buffer in meters to add on each side
             of a given sample's location when searching the planetary computer for satellite imagery.
             Defaults to 1000.
-        use_sentinel_bands (Optional[List], optional): All Sentinel-2 bands that will be used when
-            generating satellite imagery featues. Defaults to ["B02", "B03", "B04"].
-        image_feature_meter_window (Optional[int], optional): Buffer in meters on each side of a
-            given sample's location to use when creating the bounding box for the subset of a satellite
-            image that will be used to generate features. Defaults to 500.
+        use_sentinel_bands (Optional[List], optional): All Sentinel-2 bands that are needed to
+            generate satellite imagery featues. Defaults to ["B02", "B03", "B04"].
+        image_feature_meter_window (Optional[int], optional): Buffer in meters to add on each side of a
+            given sample's location when creating the bounding box for the subset of a satellite image
+            that will be used to generate features. Defaults to 500.
         n_sentinel_items (Optional[int], optional): Maximum number of Sentinel-2 items to use for each
             sample. Defaults to 1.
         satellite_image_features (Optional[List], optional): List of satellite imagery features to
             include. Defaults to [ "B02_mean", "B02_min", "B02_max", "B03_mean", "B03_min",
-            "B03_max", "B04_mean", ].
+            "B03_max", "B04_mean"].
         satellite_meta_features (Optional[List], optional): List of satellite metadata features to
             include. Defaults to [].
         metadata_features (Optional[List], optional): List of metadata features to include. Defaults
-            to ["rounded_longitude"].
+            to [].
     """
 
 
@@ -87,6 +88,8 @@ class ModelTrainingConfig(BaseModel):
     Args:
         params (Optional[LGBParams], optional): Parameters for LightGBM training. For details
             see [LightGBM's documentation](https://lightgbm.readthedocs.io/en/latest/Parameters.html).
-            Defaults to LGBParams().s
+            Defaults to LGBParams().
         num_boost_round (Optional[int], optional): Number of boosting iterations. Defaults to 1000.
+        n_folds (Optional[int], optional): Number of different model folds to train. If greater than
+            1, the models will be ensembled for a final prediction. Defaults to 1.
     """
