4abdcb3ab022783eb49d2b2ce7e3fb33fb24d2cf
Katie Wetstone <klwetstone@gmail.com>
2023-08-31 10:48:59 -0400

    add default model for cli predict

--- cyano/cli.py
+++ cyano/cli.py
@@ -1,9 +1,7 @@
 import sys
-from zipfile import ZipFile
 
 from cloudpathlib import AnyPath
 from dotenv import load_dotenv, find_dotenv
-import lightgbm as lgb
 from loguru import logger
 from pathlib import Path
 import typer
@@ -15,6 +13,8 @@ from cyano.evaluate import EvaluatePreds
 app = typer.Typer(pretty_exceptions_show_locals=False)
 
 load_dotenv(find_dotenv())
+
+REPO_ROOT = Path(__file__).parents[1].resolve()
 
 # Set logger to only log info or higher
 logger.remove()
@@ -33,20 +33,33 @@ def experiment(
 
 @app.command()
 def predict(
-    samples_path: Path = typer.Argument(
-        exists=True, help="Path to a csv of samples with columns for date, longitude, and latitude"
+    samples_path: str = typer.Argument(
+        help="Path to a csv of samples with columns for date, longitude, and latitude"
     ),
-    model_zip: Path = typer.Argument(exists=True, help="Path to a trained model zip"),
+    model_zip: str = typer.Option(
+        default=str(REPO_ROOT / "cyano/assets/model.zip"),
+        help="Path to a trained model zip",
+    ),
     output_path: Path = typer.Option(
         default="preds.csv", help="Destination to save predictions csv"
     ),
+    cache_dir: Path = typer.Option(default=None, help="Cache directory to save raw feature data"),
 ):
     """Load an existing cyanobacteria prediction model and generate
     severity level predictions for a set of samples.
     """
     samples_path = AnyPath(samples_path)
+    model_zip = AnyPath(model_zip)
 
-    pipeline = CyanoModelPipeline.from_disk(model_zip)
+    if not samples_path.exists():
+        raise FileNotFoundError(
+            f"Invalid valid for 'SAMPLES_PATH': {samples_path} does not exist."
+        )
+    if not model_zip.exists():
+        raise FileNotFoundError(f"Invalid value for 'MODEL_ZIP': {model_zip} does not exist.")
+
+    logger.info(f"Loading trained model from {model_zip}")
+    pipeline = CyanoModelPipeline.from_disk(model_zip, cache_dir=cache_dir)
     pipeline.run_prediction(samples_path, output_path)
 
 
@@ -66,12 +79,8 @@ def evaluate(
     y_pred_csv = AnyPath(y_pred_csv)
     y_true_csv = AnyPath(y_true_csv)
 
-    # Load model from model zipfile
-    archive = ZipFile(model_path, "r")
-    model = lgb.Booster(model_str=archive.read("lgb_model.txt").decode())
-
     EvaluatePreds(
-        y_pred_csv=y_pred_csv, y_true_csv=y_true_csv, save_dir=save_dir, model=model
+        y_pred_csv=y_pred_csv, y_true_csv=y_true_csv, save_dir=save_dir, model_path=model_path
     ).calculate_all_and_save()
 
 
