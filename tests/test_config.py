0730364dc9e988d4902f353aa76918eaed6b5be1
Katie Wetstone <klwetstone@gmail.com>
2023-08-31 12:08:45 -0400

    add test_experiment.py

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,7 +3,6 @@ import pytest
 
 
 from cyano.config import LGBParams, FeaturesConfig, ModelTrainingConfig
-from cyano.experiment.experiment import ExperimentConfig
 
 
 def test_lgbparams():
@@ -60,20 +59,3 @@ def test_model_training_config():
     # Errors with extra field
     with pytest.raises(ValidationError):
         ModelTrainingConfig(extra_field="surprise_extra_field")
-
-
-def test_experiment_config(train_data_path):
-    config = ExperimentConfig(
-        train_csv=train_data_path,
-        predict_csv=train_data_path,
-        features_config=FeaturesConfig(n_sentinel_items=10),
-    )
-    assert config.features_config.n_sentinel_items == 10
-
-    # Errors with extra field
-    with pytest.raises(ValidationError):
-        ExperimentConfig(
-            train_csv=train_data_path,
-            predict_csv=train_data_path,
-            extra_field="surprise_extra_field",
-        )
