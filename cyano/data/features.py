3adafd2c969b351b7e59466575ce43cb08190a57
Emily Miller <ejm714@gmail.com>
2023-09-20 00:32:07 -0700

    update comments

--- cyano/data/features.py
+++ cyano/data/features.py
@@ -97,12 +97,14 @@ def _calculate_satellite_features_for_sample_item(
                 f"Band {band} is missing from pystac item directory {sample_item_dir}"
             )
         arr = np.load(sample_item_dir / f"{band}.npy")
+        # set no data value to be nan
         band_arrays[band] = np.where(arr == 0, np.nan, arr)
 
     # Iterate over features to generate
     sample_item_features = {"sample_id": sample_id, "item_id": item_id}
 
     for feature in config.satellite_image_features:
+        # note: features will be nan if any pixel in bounding box is nan
         sample_item_features[feature] = SATELLITE_FEATURE_CALCULATORS[feature](band_arrays)
 
     return sample_item_features
@@ -203,7 +205,7 @@ def generate_all_features(
     # May be >1 row per sample, only includes samples with imagery
     satellite_features = calculate_satellite_features(satellite_meta, config, cache_dir)
 
-    # drop rows where bounding box only contained missing data
+    # drop rows where bounding box contained any missing data
     satellite_features = satellite_features.dropna()
 
     ct_with_satellite = satellite_features.index.nunique()
